1a19371bf00e3d4acdafc21bfcf5d9fff4c21c15
Joshua <joshua+github@frontlawn.net>
2023-09-20 08:39:28 -0700

    feat: Add `defineConfig` helper.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-
-
 // @ts-ignore
 declare module '@darkobits/vsct' {
 
 }
 
-export { default as Color } from './lib/color';
+export { default as Color } from 'lib/color';
+export { default } from 'lib/theme';
+import type { VSCTConfiguration } from 'etc/types';
 
-export { default } from './lib/theme';
+export function defineConfig(userConfig: VSCTConfiguration) {
+  return userConfig;
+}
+
+export type { VSCTConfiguration } from 'etc/types';
